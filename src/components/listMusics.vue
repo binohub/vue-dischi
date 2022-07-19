a7a39fbbd85c092aa9a29de0c1f04718a52c7d45
binohub <yeebino001@gmail.com>
2022-07-19 16:19:30 +0200

    spotify - axios - added

--- src/components/listMusics.vue
+++ src/components/listMusics.vue
@@ -15,13 +15,36 @@
 
 <script>
 import songElement from './songElement.vue';
+import axios from 'axios';
 
 export default {
+  data: function(){
+    return{
+
+    }
+  },
   components: {
     songElement,
+  },
+  methods:{
+    getSong(){
+      //recupero i personaggi
+      console.log("recupera i personaggi");
+      axios.get("")
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.warn(error);
+      })
+    }
+  },
+  created(){
+    this.getSong();
   }
 }
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
