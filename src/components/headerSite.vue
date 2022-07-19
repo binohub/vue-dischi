8d08d854a8df942b3038809d9217b740a77630de
binohub <yeebino001@gmail.com>
2022-07-19 15:44:58 +0200

    spotify - main - on working

--- src/components/headerSite.vue
+++ src/components/headerSite.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="d-flex align-items-center"> 
-
+    
     <div class="uwuContainer">
       <nav class="navbar navbar-dark bg-dark">
         <a class="navbar-brand" href="#">
